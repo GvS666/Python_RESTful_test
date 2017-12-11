a78f159dff5389658c3fa88fb865523fae6a7c56
Tomasz Jama <gvs666@gmail.com>
2017-12-11 19:14:10 +0000

    get and create

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 #!flask/bin/python
-from flask import Flask, jsonify, abort, request, make_response, url_for
+import uuid
+
+from flask import Flask, jsonify, request, make_response
 from flask_httpauth import HTTPBasicAuth
 from flask_pymongo import PyMongo
-
 
 app = Flask(__name__, static_url_path="")
 auth = HTTPBasicAuth()
 
+admins = {
+    "admin": "admin"
+}
+
+@auth.get_password
+def get_pw(username):
+    if username in admins:
+        return admins.get(username)
+    return None
+
 app.config['MONGO_DBNAME'] = 'restdb'
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/prtdb'
+app.config['MONGO_URI'] = 'mongodb://192.168.99.100:32769/prtdb'
 mongo_mgr = PyMongo(app)
 
 @auth.error_handler
@@ -17,6 +28,28 @@ def unauthorized():
     # auth dialog
     return make_response(jsonify({'error': 'Unauthorized access'}), 403)
 
+required = ['firstname', 'lastname', 'latitude', 'longitude']
+
+class InvalidUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
+
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
 
 @app.errorhandler(400)
 def bad_request(error):
@@ -35,13 +68,35 @@ def get_users():
     Update this to return a json stream defining a listing of the users
     Note: Always return the appropriate response for the action requested.
     """
-    return jsonify({})
+    users = mongo_mgr.db.users
+    output = []
+    for user in users.find():
+        output.append({
+            'firstname': user['firstname'],
+            'lastname': user['lastname'],
+            'latitude': user['latitude'],
+            'longitude': user['longitude'],
+            'id': user['id'],
+        })
+    return jsonify({'result' : output})
 
 
 @app.route('/prt/api/v1.0/users/<int:user_id>', methods=['GET'])
 @auth.login_required
 def get_user(user_id):
-    return jsonify({})
+    users = mongo_mgr.db.users
+    output = []
+    user = users.find_one({'id': str(user_id)})
+    if not user:
+        raise InvalidUsage('User with ID %d not found' % user_id , status_code=404)
+    output.append({
+        'firstname': user['firstname'],
+        'lastname': user['lastname'],
+        'latitude': user['latitude'],
+        'longitude': user['longitude'],
+        'id': user['id'],
+    })
+    return jsonify({'result': output})
 
 
 @app.route('/prt/api/v1.0/users', methods=['POST'])
@@ -51,7 +106,21 @@ def create_user():
     Should add a new user to the users collection, with validation
     note: Always return the appropriate response for the action requested.
     """
-    return jsonify({})
+    users = mongo_mgr.db.users
+    request_data = request.get_json()
+    missing = []
+    data = {}
+    for r in required:
+        if r not in request_data or not request_data[r]:
+            missing.append(r)
+        else:
+            data[r] = request_data[r]
+    if missing:
+        raise InvalidUsage('Missing user data: %s' % ', '.join(missing) , status_code=400)
+    data['id'] = str(uuid.uuid4().int)
+    users.insert(data)
+    del data['_id']
+    return jsonify({'result': data}), 201
 
 
 @app.route('/prt/api/v1.0/users/<int:user_id>', methods=['PUT'])
